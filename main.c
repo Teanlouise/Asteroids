107b05124d42e2a5c706b9e35eec0e69f47f3d72
Tean McGrath <teanmcgrath@gmail.com>
2019-05-15 19:40:04 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * CSSE2010_Project_15.05.c
+ *
+ * Created: 5/15/2019 7:16:37 PM
+ * Author : Todd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
